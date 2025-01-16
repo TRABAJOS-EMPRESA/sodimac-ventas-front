321906677d96c16aee809394464852b3d3c40e32
TITANRAH <granrah1@gmail.com>
2025-01-16 09:29:12 -0300

    bajada de version a next 14 y react 18

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
+import { Roboto } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  weight: "400",
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata: Metadata = {
   title: "Mi Radar VVEE",
-  description:
-    "Herramienta para la gestión de Oportunidades",
+  description: "Herramienta para la gestión de Oportunidades",
   icons: {
     icon: "/img/mi_radar_only_icon.svg",
   },
@@ -31,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary-white`}
-      >
+      <body className={`${roboto.className} antialiased bg-primary-white`}>
         {children}
         <Toaster />
       </body>
